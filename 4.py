38d58c3e0f47be1a96f2c0fdd9ba7663313bd99d
carsongmiller <carsongmiller@gmail.com>
2021-12-06 17:54:06 -0600

    Part 4 complete

---
+++ 4.py
@@ -0,0 +1,115 @@
+lines = str()
+with open('4_input.txt') as f:
+	lines = [n.strip() for n in f.readlines()]
+
+
+
+def vertWin(board):
+	for i in range(len(board[0])):
+		# print(board)
+		# print(i)
+		col = set([item[i] for item in board])
+		if len(col) == 1 and '' in col: return True
+	return False
+
+def horiWin(board):
+	for i in range(len(board)):
+		col = set(board[i])
+		if len(col) == 1 and '' in col: return True
+	return False
+
+
+board = [
+['','','56','',''],
+['60','','','','96'],
+['','','','',''],
+['','','9','',''],
+['','29','','5','']
+]
+print(horiWin(board))
+
+def part1(lines):
+
+	calls = lines[0].split(',')
+
+	boards = []
+
+	lines = lines[1:]
+	lines.remove('')
+
+	boards.append([])
+	for i in range(len(lines)):
+		if i % 6 == 5:
+			boards.append([])
+			continue
+		boards[-1].append(lines[i].split())
+	callsRequired = []
+
+	for b in range(len(boards)):
+		board = boards[b]
+		winFound = False
+		for c in range(len(calls)):
+			call = calls[c]
+			for row in range(len(board)):
+				for cell in range(len(board[row])):
+					if board[row][cell] == call:
+						board[row][cell] = ''
+						if vertWin(board) or horiWin(board):
+							callsRequired.append(c + 1)
+							winFound = True
+					if winFound: break
+				if winFound: break
+			if winFound: break
+		
+	winner = callsRequired.index(min(callsRequired))
+	sum = 0
+	for row in boards[winner]:
+		for cell in row:
+			if cell != '': sum += int(cell)
+
+	return (sum * int(calls[min(callsRequired) - 1]))
+
+def part2(lines):
+
+	calls = lines[0].split(',')
+	boards = []
+
+	lines = lines[1:]
+	lines.remove('')
+
+	boards.append([])
+	for i in range(len(lines)):
+		if i % 6 == 5:
+			boards.append([])
+			continue
+		boards[-1].append(lines[i].split())
+
+	callsRequired = []
+
+	for b in range(len(boards)):
+		board = boards[b]
+		winFound = False
+		for c in range(len(calls)):
+			call = calls[c]
+			for row in range(len(board)):
+				for cell in range(len(board[row])):
+					if board[row][cell] == call:
+						board[row][cell] = ''
+						if vertWin(board) or horiWin(board):
+							callsRequired.append(c + 1)
+							winFound = True
+							break
+					if winFound: break
+				if winFound: break
+			if winFound: break
+		
+	loser = callsRequired.index(max(callsRequired))
+	sum = 0
+	for row in boards[loser]:
+		for cell in row:
+			if cell != '': sum += int(cell)
+	return sum * int(calls[max(callsRequired) - 1])
+
+
+print("Part 1:", part1(lines))
+print("Part 2:", part2(lines))
