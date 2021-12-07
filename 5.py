54a81d3027642ea9bb02ab2bedc81f436743e0db
carsongmiller <carsongmiller@gmail.com>
2021-12-07 11:29:53 -0600

    Finished day 6 (but not 5...)

--- 5.py
+++ 5.py
@@ -1,3 +1,13 @@
 lines = str()
 with open('5_input.txt') as f:
 	lines = [n.strip() for n in f.readlines()]
+
+def part1(lines):
+	pass
+
+def part2(lines):
+	pass
+
+
+print("Part 1:", part1(lines))
+print("Part 2:", part2(lines))
---
+++ 6.py
@@ -0,0 +1,62 @@
+lines = str()
+with open('6_input.txt') as f:
+	lines = [n.strip() for n in f.readlines()]
+
+def stepDay(fishDict):
+	newFishDict = {}
+	for key in fishDict:
+		if key == 0:
+			newFishDict[8] = fishDict[key]
+			if 6 not in newFishDict:
+				newFishDict[6] = fishDict[key]
+			else:
+				newFishDict[6] += fishDict[key]
+		else:
+			if key-1 not in newFishDict:
+				newFishDict[key-1] = fishDict[key]
+			else:
+				newFishDict[key-1] += fishDict[key]
+	return newFishDict
+
+def part1(lines):
+	fishes = [int(fish) for fish in lines[0].split(',')]
+
+	fishDict = {}
+	for fish in fishes:
+		if fish not in fishDict:
+			fishDict[fish] = 1
+		else:
+			fishDict[fish] += 1
+	
+	for i in range(80):
+		fishDict = stepDay(fishDict)
+		
+		# print(fishDict)
+		# sum = 0
+		# for key in fishDict:
+		# 	sum += fishDict[key]
+		# print(sum)
+	
+	sum = 0
+	for key in fishDict:
+		sum += fishDict[key]
+	return sum
+
+def part2(lines):
+	fishes = [int(fish) for fish in lines[0].split(',')]
+	fishDict = {}
+	for fish in fishes:
+		if fish not in fishDict:
+			fishDict[fish] = 1
+		else:
+			fishDict[fish] += 1
+	for i in range(256):
+		fishDict = stepDay(fishDict)
+	sum = 0
+	for key in fishDict:
+		sum += fishDict[key]
+	return sum
+
+
+print("Part 1:", part1(lines))
+print("Part 2:", part2(lines))
