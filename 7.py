d076a9ed7d404bfdc845285155b0fe7b68c2a7e4
carsongmiller <carsongmiller@gmail.com>
2021-12-07 13:08:37 -0600

    Day 7 complete

---
+++ 7.py
@@ -0,0 +1,51 @@
+lines = str()
+with open('7_input.txt') as f:
+	lines = [n.strip() for n in f.readlines()]
+
+def triangleNum(n):
+	if n == 0:
+		return n
+	i = n
+	while True:
+		if i == 1:
+			return n
+		i -= 1
+		n += i
+
+def part1(lines):
+	crabs = [int(x) for x in lines[0].split(',')]
+	bestDist = max(crabs) * len(crabs)
+	bestLoc = 0
+	for i in range(min(crabs), max(crabs) + 1):
+		travelDist = 0
+		for crab in crabs:
+			travelDist += abs(crab - i)
+		if travelDist < bestDist:
+			bestDist = travelDist
+			bestLoc = i
+	return bestDist
+
+def part2(lines):
+	crabs = [int(x) for x in lines[0].split(',')]
+	
+
+	#precompute all the triangle numbers we may need, store in dict
+	triangles = {}
+	for i in range(max(crabs) + 1):
+		triangles[i] = triangleNum(i)
+
+	bestDist = triangles[max(crabs)] * len(crabs)
+	bestLoc = 0
+
+	for i in range(min(crabs), max(crabs) + 1):
+		travelDist = 0
+		for crab in crabs:
+			travelDist += triangles[abs(crab - i)]
+		if travelDist < bestDist:
+			bestDist = travelDist
+			bestLoc = i
+	return bestDist
+
+
+print("Part 1:", part1(lines))
+print("Part 2:", part2(lines))
